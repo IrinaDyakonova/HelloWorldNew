02803c0594abf60d8934dde94fb34145c33a2cef
IrinaDyakonova <dyakonova.irina.v@gmail.com>
2020-03-24 21:50:29 +0200

    by PR

--- src/HelloWorldMaster.java
+++ src/HelloWorldMaster.java
@@ -1,6 +1,7 @@
 public class HelloWorldMaster {
     public static void main(String [] args)
     {
+        // by PR
         System.out.println("Hello world");
         System.out.println("Hello mates");
     }
